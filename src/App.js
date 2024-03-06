5ee8a997392af687ef5d081c577d26a9b3dabf92
schinak-m <shinak.x3@gmail.com>
2024-03-06 19:46:47 +0300

    changed BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'; // Import your shared Navbar component
 import Countries from './components/Countries';
 import CountryDetails from './components/CountryDetails';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 
 export default function App() {
   const [darkMode, setDarkMode] = React.useState(false);
@@ -18,12 +19,12 @@ export default function App() {
   return (
     <>
       <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Countries/>}></Route>
           <Route path="/:name" element={<CountryDetails/>}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
